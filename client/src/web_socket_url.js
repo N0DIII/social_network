9f350a326a03a663416d9bf69f2074bc01e329ca
N0DIII <105385715+N0DIII@users.noreply.github.com>
2024-04-08 19:22:25 +0300

    14

--- client/src/web_socket_url.js
+++ client/src/web_socket_url.js
@@ -1,3 +1,4 @@
 //module.exports = 'wss://192.168.31.200:5000';
 //module.exports = 'http://localhost:4000';
-module.exports = 'http://26.47.4.9:4000';
+//module.exports = 'http://26.47.4.9:4000';
+module.exports = 'https://volsoc.ru:4000';
